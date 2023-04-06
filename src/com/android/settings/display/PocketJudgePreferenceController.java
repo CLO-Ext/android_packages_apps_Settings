65bf9e37b9c41782babcb50f67af4e32dd8184b9
Dominik Procházka <dominikprochazka100@gmail.com>
2023-04-06 22:14:51 +0200

    Settings: Enhance Pocket Judge [2/2]
    Add options that allow users to choose what buttons to block in pocket mode.
    Also convert the Pocket Judge preference to PrimarySwitchPreference

--- src/com/android/settings/display/PocketJudgePreferenceController.java
+++ src/com/android/settings/display/PocketJudgePreferenceController.java
@@ -1,65 +1,107 @@
 /*
- * Copyright (C) 2018 Paranoid Android
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
+ * Copyright (C) 2022 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
  */
+
 package com.android.settings.display;
 
-import static android.provider.Settings.System.POCKET_JUDGE;
-
+import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
 import android.provider.Settings;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
-import com.android.settings.DisplaySettings;
-import com.android.settings.core.PreferenceControllerMixin;
-import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
 
-import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.PrimarySwitchPreference;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;	
 
-import org.zeph.support.preference.SwitchPreference;
+public class PocketJudgePreferenceController extends TogglePreferenceController
+        implements LifecycleObserver, OnStart, OnStop {
 
-public class PocketJudgePreferenceController extends AbstractPreferenceController implements
-        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+    private static final String KEY = "pocket_judge";
 
-    private static final String KEY_POCKET_JUDGE = "pocket_judge";
+    private PrimarySwitchPreference mPreference;
+    private SettingObserver mSettingObserver;
 
-    public PocketJudgePreferenceController(Context context) {
-        super(context);
+    public PocketJudgePreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
     }
 
     @Override
-    public String getPreferenceKey() {
-        return KEY_POCKET_JUDGE;
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+        mSettingObserver = new SettingObserver(mPreference);
     }
 
     @Override
-    public void updateState(Preference preference) {
-        int pocketJudgeValue = Settings.System.getInt(mContext.getContentResolver(),
-                POCKET_JUDGE, 0);
-        ((SwitchPreference) preference).setChecked(pocketJudgeValue != 0);
+    public boolean isChecked() {
+        return Settings.System.getInt(mContext.getContentResolver(), KEY, 0) == 1;
     }
 
     @Override
-    public boolean isAvailable() {
-        return mContext.getResources().getBoolean(
+    public boolean setChecked(boolean isChecked) {
+        return Settings.System.putInt(mContext.getContentResolver(), KEY, isChecked ? 1 : 0);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        boolean isAvailable = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_pocketModeSupported);
+        return isAvailable ? 1 : 0;
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        boolean pocketJudgeValue = (Boolean) newValue;
-        Settings.System.putInt(mContext.getContentResolver(), POCKET_JUDGE, pocketJudgeValue ? 1 : 0);
-        return true;
+    public void onStart() {
+        if (mSettingObserver != null) {
+            mSettingObserver.register(mContext.getContentResolver());
+            mSettingObserver.onChange(false, null);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (mSettingObserver != null) {
+            mSettingObserver.unregister(mContext.getContentResolver());
+        }
+    }
+
+    private class SettingObserver extends ContentObserver {
+        private final Uri mUri = Settings.System.getUriFor(KEY);
+
+        private final Preference mPreference;
+
+        SettingObserver(Preference preference) {
+            super(new Handler());
+            mPreference = preference;
+        }
+
+        public void register(ContentResolver cr) {
+            cr.registerContentObserver(mUri, false, this);
+        }
+
+        public void unregister(ContentResolver cr) {
+            cr.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri == null || mUri.equals(uri)) {
+                updateState(mPreference);
+            }
+        }
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
     }
 }
