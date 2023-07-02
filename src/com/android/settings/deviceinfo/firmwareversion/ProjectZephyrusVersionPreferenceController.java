14276fd697f7d5bf345fdaf7955f63679fa4c22f
HELLBOY017 <abhaygill017@gmail.com>
2023-07-02 08:39:24 +0530

    Settings: aospa -> zeph

--- src/com/android/settings/deviceinfo/firmwareversion/ProjectZephyrusVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/ProjectZephyrusVersionPreferenceController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021 Paranoid Android
+ * Copyright 2016-2021 Project Zephyrus
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,15 +22,15 @@ import android.os.SystemProperties;
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
-public class ParanoidAndroidVersionPreferenceController extends BasePreferenceController {
+public class ProjectZephyrusVersionPreferenceController extends BasePreferenceController {
 
-    private static final String AOSPA_BUILD_VARIANT_PROP = "ro.aospa.build.variant";
-    private static final String AOSPA_VERSION_MAJOR_PROP = "ro.aospa.version.major";
-    private static final String AOSPA_VERSION_MINOR_PROP = "ro.aospa.version.minor";
+    private static final String ZEPH_BUILD_VARIANT_PROP = "ro.zeph.build.variant";
+    private static final String ZEPH_VERSION_MAJOR_PROP = "ro.zeph.version.major";
+    private static final String ZEPH_VERSION_MINOR_PROP = "ro.zeph.version.minor";
 
     private final Context mContext;
 
-    public ParanoidAndroidVersionPreferenceController(Context context, String key) {
+    public ProjectZephyrusVersionPreferenceController(Context context, String key) {
         super(context, key);
         mContext = context;
     }
@@ -42,19 +42,19 @@ public class ParanoidAndroidVersionPreferenceController extends BasePreferenceCo
 
     @Override
     public CharSequence getSummary() {
-        String aospaVersionMajor = SystemProperties.get(AOSPA_VERSION_MAJOR_PROP,
+        String zephVersionMajor = SystemProperties.get(ZEPH_VERSION_MAJOR_PROP,
                 mContext.getResources().getString(R.string.device_info_default));
-        String aospaVersionMinor = SystemProperties.get(AOSPA_VERSION_MINOR_PROP,
+        String zephVersionMinor = SystemProperties.get(ZEPH_VERSION_MINOR_PROP,
                 mContext.getResources().getString(R.string.device_info_default));
-        String aospaBuildVariant = SystemProperties.get(AOSPA_BUILD_VARIANT_PROP,
+        String zephBuildVariant = SystemProperties.get(ZEPH_BUILD_VARIANT_PROP,
                 mContext.getResources().getString(R.string.device_info_default));
 
-        if (aospaBuildVariant.equals("Release")) {
-            return aospaVersionMajor + " " + aospaVersionMinor;
-        } else if (aospaBuildVariant.equals("Unofficial")) {
-           return aospaVersionMajor + " " + aospaBuildVariant;
+        if (zephBuildVariant.equals("Release")) {
+            return zephVersionMajor + " " + zephVersionMinor;
+        } else if (zephBuildVariant.equals("Unofficial")) {
+           return zephVersionMajor + " " + zephBuildVariant;
         } else {
-           return aospaVersionMajor + " " + aospaBuildVariant + " " + aospaVersionMinor;
+           return zephVersionMajor + " " + zephBuildVariant + " " + zephVersionMinor;
         }
     }
 }
