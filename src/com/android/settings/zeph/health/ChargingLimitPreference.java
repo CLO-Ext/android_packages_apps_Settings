f1dae9106387e4228898dac93eb22443dc43f479
HELLBOY017 <abhaygill017@gmail.com>
2023-09-09 13:26:03 +0530

    Settings: Use CustomSeekBarPreference for charging limit

--- src/com/android/settings/zeph/health/ChargingLimitPreference.java
+++ src/com/android/settings/zeph/health/ChargingLimitPreference.java
@@ -18,67 +18,31 @@ package com.android.settings.zeph.health;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.SeekBar;
-import android.widget.TextView;
-
-import androidx.preference.Preference;
-import androidx.preference.PreferenceViewHolder;
 
 import com.android.internal.lineage.health.HealthInterface;
 
-import com.android.settings.R;
+import org.zeph.support.preference.CustomSeekBarPreference;
 
-public class ChargingLimitPreference extends Preference
-        implements SeekBar.OnSeekBarChangeListener {
+public class ChargingLimitPreference extends CustomSeekBarPreference {
     private static final String TAG = ChargingLimitPreference.class.getSimpleName();
 
-    private TextView mChargingLimitValue;
-    private SeekBar mChargingLimitBar;
+    private static int mMinVal = 70;
 
     private final HealthInterface mHealthInterface;
 
     public ChargingLimitPreference(final Context context, final AttributeSet attrs) {
         super(context, attrs);
 
-        setLayoutResource(R.layout.preference_charging_limit);
-
+        mUnits = "%";
+        mMinValue = mMinVal;
+        mDefaultValueExists = true;
         mHealthInterface = HealthInterface.getInstance(context);
+        mDefaultValue = mHealthInterface.getLimit();
     }
 
     @Override
-    public void onBindViewHolder(final PreferenceViewHolder holder) {
-        super.onBindViewHolder(holder);
-
-        mChargingLimitValue = (TextView) holder.findViewById(R.id.value);
-
-        mChargingLimitBar = (SeekBar) holder.findViewById(R.id.seekbar_widget);
-        mChargingLimitBar.setOnSeekBarChangeListener(this);
-
-        int currLimit = getSetting();
-        mChargingLimitBar.setProgress(currLimit);
-        updateValue(currLimit);
-    }
-
-    @Override
-    public void onStartTrackingTouch(final SeekBar seekBar) {
-    }
-
-    @Override
-    public void onStopTrackingTouch(final SeekBar seekBar) {
-        setSetting(seekBar.getProgress());
-    }
-
-    @Override
-    public void onProgressChanged(final SeekBar seekBar, final int progress,
-            final boolean fromUser) {
-        updateValue(progress);
-    }
-
-    public void setValue(final int value) {
-        if (mChargingLimitBar != null) {
-            mChargingLimitBar.setProgress(value);
-        }
-        updateValue(value);
+    protected void changeValue(int newValue) {
+        mHealthInterface.setLimit(newValue);
     }
 
     protected int getSetting() {
@@ -87,11 +51,5 @@ public class ChargingLimitPreference extends Preference
 
     protected void setSetting(final int chargingLimit) {
         mHealthInterface.setLimit(chargingLimit);
-    }
-
-    private void updateValue(final int value) {
-        if (mChargingLimitValue != null) {
-            mChargingLimitValue.setText(String.format("%d%%", value));
-        }
     }
 }
