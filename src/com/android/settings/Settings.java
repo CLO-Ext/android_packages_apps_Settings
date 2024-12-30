7fdac1b5206baa75959ff4f75183a312af6e763c
Abhay Singh Gill <abhaygill017@gmail.com>
2024-12-30 22:55:35 +0100

    Settings: Add Battery Share settings
    * The Quick Settings tile for enabling Battery Share was unintuitive, providing no context or explanation about its functionality. This update introduces a settings fragment that allows users to toggle the Battery Share feature on and off while providing a description of what the feature does, improving clarity and usability.
    
    Co-authored-by: Akash Srivastava <akashniki@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -540,4 +540,5 @@ public class Settings extends SettingsActivity {
     public static class HearingDevicesPairingActivity extends SettingsActivity { /* empty */ }
 
     public static class HeadsUpSettingsActivity extends SettingsActivity {}
+    public static class BatteryShareSettingsActivity extends SettingsActivity {}
 }
